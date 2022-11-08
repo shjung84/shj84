25d954afc3e10a92c85d7232ac4ca088c1d8c821
shjung <seunghyun.jung.84@gmail.com>
2022-11-08 11:11:50 +0900

    Put Test

--- pages/src/components/HelloWorld.vue
+++ pages/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang="pug">
+div.hello
+  h1 {{ msg }}
 </template>
 
 <script>
@@ -14,19 +13,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
